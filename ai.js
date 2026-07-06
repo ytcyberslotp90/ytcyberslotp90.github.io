363fcbf1e9d404cdc345c681a8a181dd85e7a303
ytcyberslotp90 <krishnendumaji3633@gmail.com>
2026-07-06 12:39:27 +0530

    Add files via upload

---
+++ ai.js
@@ -0,0 +1,8 @@
+(function() {
+  var s = document.createElement("script");
+  s.src = "https://widget.cxgenie.ai/widget.js";
+  s.setAttribute("data-aid", "39747433-3d45-478f-b4c1-e954e33379a2");
+  s.setAttribute("data-lang", "en");
+  s.async = true;
+  document.body.appendChild(s);
+})();
---
+++ matrix.js
@@ -0,0 +1,39 @@
+    const canvas = document.getElementById("matrix");
+    const ctx = canvas.getContext("2d");
+
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+
+    const letters = "01";
+    const fontSize = 16;
+    const columns = canvas.width / fontSize;
+
+    const drops = [];
+    for (let x = 0; x < columns; x++) {
+      drops[x] = 1;
+    }
+
+    function draw() {
+      ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+      ctx.fillStyle = "rgba(21, 255, 0, 1)";
+      ctx.font = fontSize + "px monospace";
+
+      for (let i = 0; i < drops.length; i++) {
+        const text = letters.charAt(Math.floor(Math.random() * letters.length));
+        ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+
+        if (drops[i] * fontSize > canvas.height && Math.random() > 0.975) {
+          drops[i] = 0;
+        }
+        drops[i]++;
+      }
+    }
+
+    setInterval(draw, 35);
+
+    window.addEventListener("resize", () => {
+      canvas.height = window.innerHeight;
+      canvas.width = window.innerWidth;
+    });
